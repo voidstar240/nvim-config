2da4fade7419fd69af8d9e9e47f7873cc29db401
Andrew Freuler <andrewfreuler@gmail.com>
2024-05-27 01:10:34 -0400

    Disable relative line numbers.

--- lua/andrew/set.lua
+++ lua/andrew/set.lua
@@ -2,8 +2,7 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.opt.nu = true                -- shows the current line number at the cursor
-vim.opt.relativenumber = true    -- shows relative line number above and below the cursor
+vim.opt.nu = true                -- shows the line number column
 
 vim.opt.tabstop = 4              -- number of spaces per tab
 vim.opt.softtabstop = 0          -- number of spaces per tab during editing
@@ -11,7 +10,7 @@ vim.opt.shiftwidth = 0           -- number of spaces to use for auto indent
 vim.opt.expandtab = true         -- insert spaces instead of tabs
 
 vim.opt.autoindent = true        -- match indentation to preceding line
-vim.opt.smartindent = true       -- extra rules to govern above
+vim.opt.smartindent = true       -- extra rules to govern the above
 
 vim.opt.wrap = false             -- text wrapping
 
